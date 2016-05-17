ec5cbadad0b38699cb8f9967c2b99607ddc0eea0
Mister <200739491@qq.com>
2016-05-17 11:13:39 +0800

    0.0.9

--- Pods/Target Support Files/Pods-LSApplication/Pods-LSApplication-umbrella.h
+++ Pods/Target Support Files/Pods-LSApplication/Pods-LSApplication-umbrella.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+#import "LSApplicationProxy.h"
+#import "LSApplicationWorkspace.h"
+
 
 FOUNDATION_EXPORT double Pods_LSApplicationVersionNumber;
 FOUNDATION_EXPORT const unsigned char Pods_LSApplicationVersionString[];
